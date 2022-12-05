922717f04adb125d3987b3648e043338835b5c76
Vinaykumar Reddy Kakanuru <31994314+vinaykakanuru@users.noreply.github.com>
2022-12-05 15:44:26 +0530

    completed module5

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -5,6 +5,7 @@ from load_data import load_sensor_data
 from temperature_info import TemperatureData
 from humidity_info import HumidityData
 from particle_count_info import ParticleData
+from energy_info import EnergyData
 # Runner script for all modules
 
 
@@ -76,3 +77,16 @@ concentrations = particle_data.get_data_concentrations(data=recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
+energy_data = EnergyData(data)
+recs = energy_data.get_data_by_area(rec_area = test_area)
+print("\nHouse Energy sensor records for area {} = {}".format(test_area, len(recs)))
+
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
+
+recs = energy_data.get_data_by_date(rec_date = test_date)
+print("House Energy sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
+
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
